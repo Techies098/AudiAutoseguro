8728ab45ac1ab1dc445ee8363b39f1155b475b9e
Ricardo <rjnv098@gmail.com>
2024-05-01 16:04:47 -0400

    migracion y modelo

---
+++ database/migrations/2024_05_01_191605_create_userlogs_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('userlogs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('action');
+            $table->timestamp('datetime');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('userlogs');
+    }
+};
