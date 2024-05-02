f8564d2b6c110bc973238e9513fca6daf2f82d9c
Alex Orellana <lexorel88@gmail.com>
2024-05-02 02:34:17 -0400

    Se avanso una parte

---
+++ app/Livewire/Vehiculosrep/ListaVehiculosrep.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Livewire\Vehiculosrep;
+
+use App\Models\Vehiculo;
+use Barryvdh\DomPDF\Facade\Pdf as PDF;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class ListaVehiculosrep extends Component
+{
+    use WithPagination;
+
+    public $buscar, $placa = 'placa';
+
+    public function render()
+    {
+        $vehiculos = $this->buscar ? $this->buscarVehiculos() : $this->obtenerVehiculos();
+
+        return view('livewire.vehiculosrep.lista-vehiculosrep', compact('vehiculos'));
+    }
+
+    public function buscarVehiculos()
+    {
+        $this->validate([
+            'buscar' => 'required|string|min:1'
+        ]);
+
+        return Vehiculo::where('' . $this->placa . '', 'like', '%' . trim($this->buscar) . '%')
+            ->orderBy('' . $this->placa . '', 'asc')
+            ->paginate(25);
+    }
+
+    private function obtenerVehiculos()
+    {
+        return Vehiculo::orderBy('' . $this->placa . '', 'asc')
+            ->paginate(25);
+    }
+
+    public function generarReporte()
+    {
+        $vehiculos = Vehiculo::all();
+        $pdf = PDF::loadView('reporte.vehiculos.pdf-result', compact('vehiculos'));
+
+        return $pdf->stream();
+    }
+
+    /*public function render()
+    {
+        return view('livewire.vehiculosrep.lista-vehiculosrep');
+    }*/
+}
