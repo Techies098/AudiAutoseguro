ea5cb7f8d7df4b3cb890e553d8a1c94465c44c14
Benjamin Romero <programador.ben@gmail.com>
2024-05-07 03:03:01 -0400

    Cambio de nombres mayusculula Administrador, bitacora

--- resources/views/Administrador/permisos/lista-permisos.blade.php
+++ resources/views/Administrador/permisos/lista-permisos.blade.php
@@ -1,20 +1,3 @@
-{{-- <div>
-    <ul class="list-group list-group-flush">
-        <form action="{{ route('administrador/permisos.guardar', ['user' => $user->id]) }}" method="POST">
-            
-            @foreach ($permissions as $permission)
-                <li class="list-group-item">
-                    <input class="form-check-input mr-2" type="checkbox" value="{{ $permission->id }}" id="permission_{{ $permission->id }}" name="permissions[]" @if(in_array($permission->id, $allUserPermissions)) checked disabled @endif>
-                    <label class="form-check-label" for="permission_{{ $permission->id }}">{{ $permission->name . ' - ' . $permission->description }}</label>
-                </li>
-            @endforeach
-            <br>
-                @csrf
-            <button type="submit" class="btn btn-primary">Guardar</button>
-        </form>
-    </ul>
-</div> --}}
-
 <div>
     <ul class="list-group list-group-flush">
         <form action="{{ route('administrador/permisos.guardar', ['user' => $user->id]) }}" method="POST">
