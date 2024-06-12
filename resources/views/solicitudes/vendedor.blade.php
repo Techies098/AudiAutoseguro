ec547ddab9f228189347c909d68d1437cb41890c
GBAC22 <gbac2208@gmail.com>
2024-06-12 00:18:34 -0400

    correcciones

--- resources/views/solicitudes/vendedor.blade.php
+++ resources/views/solicitudes/vendedor.blade.php
@@ -21,46 +21,6 @@
                         </div>
                     </div>
 
-                    <div class="col-md-12">
-                        <h2>Solicitudes Pendientes</h2>
-
-                        <div class="table-responsive">
-                            <table class="table table-bordered table-striped table-rounded">
-                                <thead class="table-dark">
-                                    <tr>
-                                        <th scope="col">ID</th>
-                                        <th scope="col">Cliente</th>
-                                        <th scope="col">Seguro</th>
-                                        <th scope="col">Estado</th>
-                                        <th scope="col">Fecha</th>
-                                        <th scope="col">Hora</th>
-                                        <th scope="col">Acciones</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($pendientes as $solicitud)
-                                    <tr>
-                                        <td>{{ $solicitud->id }}</td>
-                                        <td>{{ $solicitud->user->name }}</td>
-                                        <td>{{ $solicitud->seguro->nombre }}</td>
-                                        <td>{{ $solicitud->estado }}</td>
-                                        <td>{{ $solicitud->fecha }}</td>
-                                        <td>{{ $solicitud->hora }}</td>
-                                        <td>
-                                            <form action="{{ route('solicitudes.cambiarEstado', $solicitud->id) }}" method="POST">
-                                                @csrf
-                                                @method('PATCH')
-                                                <button type="submit" name="estado" value="en progreso" class="btn btn-warning">En Progreso</button>
-                                                <button type="submit" name="estado" value="denegada" class="btn btn-danger">Denegar</button>
-                                            </form>
-                                                <a href="{{ route('solicitudes.show', $solicitud->id) }}" class="btn btn-primary">Ver Detalles</a>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
 
                     <div class="col-md-12">
                         <h2>Solicitudes En Progreso</h2>
